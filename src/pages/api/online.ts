c852b7527ba3fde8cb038f0415b5c395737c69ae
Kieran <51853889+tiredwithoutcoffee@users.noreply.github.com>
2024-04-03 17:57:57 +0100

    updated user ID

--- src/pages/api/online.ts
+++ src/pages/api/online.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const response = await fetch(
-    `https://api.lanyard.rest/v1/users/465094881908621313`
+    `https://api.lanyard.rest/v1/users/998242055644008459`
   ).then((res) => res.json());
 
   res.status(200).json(response);
